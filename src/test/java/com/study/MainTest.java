af0ed00146fda73d95110eab0e6c2b04fe5d83b6
roy <673713797@qq.com>
2017-08-08 09:50:51 +0800

    main submit

--- src/test/java/com/study/MainTest.java
+++ src/test/java/com/study/MainTest.java
@@ -1,25 +1,25 @@
-package com.study;
-
-import com.study.properties.QuNiuService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * Created by roy on 2017/7/31.
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MainTest {
-    @Autowired
-    QuNiuService quNiuService;
-
-    @Test
-    public void qiniu() {
-        String token = quNiuService.getQiNiuToken();
-        System.out.println("qiNiu== "+token);
-
-    }
-}
+//package com.study;
+//
+//import com.study.properties.QuNiuService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+///**
+// * Created by roy on 2017/7/31.
+// */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class MainTest {
+//    @Autowired
+//    QuNiuService quNiuService;
+//
+//    @Test
+//    public void qiniu() {
+//        String token = quNiuService.getQiNiuToken();
+//        System.out.println("qiNiu== "+token);
+//
+//    }
+//}
